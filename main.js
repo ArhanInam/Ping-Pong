5c8a48bbbcb18764517c1cba5545d83c5bfc385d
Arhan Inam <91776437+ArhanInam@users.noreply.github.com>
2023-06-25 15:36:25 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -26,6 +26,11 @@ function preload()
 
 function setup(){
   var canvas =  createCanvas(700,600);
+  canvas.parent("canvas");
+  video = createCapture(VIDEO);
+  video.size(700,600);
+  video.hide();
+  poseNet = ml5.poseNet(video, modelLoaded);
 }
 
 
