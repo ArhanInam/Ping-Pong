069925ec790a1e3071ec8bc058fd39640cd1754f
Arhan Inam <91776437+ArhanInam@users.noreply.github.com>
2023-07-09 13:56:11 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -35,7 +35,6 @@ function setup(){
   canvas.parent("canvas");
   video = createCapture(VIDEO);
   video.size(700,600);
-  video.hide();
   poseNet = ml5.poseNet(video, modelLoaded);
   poseNet.on('pose',gotPoses);
 }
@@ -43,9 +42,7 @@ function setup(){
 
 function draw(){
 
- image(video, 0, 0, 600, 500);
-
- background(platform); 
+ background(platform);
 
  fill("black");
  stroke("black");
